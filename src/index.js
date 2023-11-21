81cffe34a3891ef6c46c56f0b62dccc32df6de31
Ishii <ismaelv@protonmail.com>
2023-11-21 01:02:06 -0800

    Added the feature to switch between projects

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './style.css';
 const app = (function() {
     const projects = [];
+    let viewingProject = 0;
     function Project(name) {
         class TodoItem {
             constructor(title, description, dueDate, priority) {
@@ -29,7 +30,14 @@ const app = (function() {
     function generateSidebar(project, id) {
         const projectTab = document.createElement('div');
         projectTab.classList.toggle('project-view');
+        projectTab.setAttribute('data-project-id', id);
         projectTab.innerHTML = `${project.getName()}`;
+        projectTab.addEventListener('click', () => {
+            dom.resetDashboard();
+            project.todos.forEach((todo) => {
+                dom.getDashboard().appendChild(GenerateTodoCard(todo));
+            });
+        });
         return projectTab;
     }
 
@@ -45,12 +53,21 @@ const app = (function() {
         return todoCard;
     }
 
-    function updateDisplay() {
+    function showAllTodos() {
         getProjects().forEach((project, id) => {
             dom.sidebar.appendChild(generateSidebar(project, id));
             project.todos.forEach((todo) => {
-                dom.dashboard.appendChild(GenerateTodoCard(todo));
+                dom.getDashboard().appendChild(GenerateTodoCard(todo));
             });
+        });
+    }
+
+    function initiateDisplay() {
+        getProjects().forEach((project, id) => {
+            dom.sidebar.appendChild(generateSidebar(project, id));
+        });
+        projects[0].todos.forEach((todo) => {
+            dom.getDashboard().appendChild(GenerateTodoCard(todo));
         });
     }
 
@@ -68,13 +85,14 @@ const app = (function() {
     addProject('default');
     addProject('second!');
     projects[0].addTodo("First Task", "Our first thing to do!", "Tomorrow", "high");
-    return { updateDisplay, addProject, generateSidebar, getLastProject, getProjects, appendProjectSidebar, GenerateTodoCard };
+    projects[1].addTodo("Second Task", "Our second thing to do!", "Tomorrow", "low");
+    return { initiateDisplay, addProject, generateSidebar, getLastProject, getProjects, appendProjectSidebar, GenerateTodoCard };
 })();
 
 const dom = (function () {
     const body = document.getElementsByTagName('body')[0]; 
     const sidebar = document.getElementById('sidebar');
-    const dashboard = document.getElementById('dashboard');
+    let dashboard = document.getElementById('dashboard');
     const modalAddProject = document.getElementById('modal-add-project');
     const modalAddTodo = document.getElementById('modal-add-todo');
     const buttonAddProject = document.getElementById('button-add-project');
@@ -83,12 +101,25 @@ const dom = (function () {
     const formAddProject = document.querySelector('#form-add-project');
     const formAddTodo = document.getElementById('form-add-todo');
 
-    const buttonAddTodo = document.createElement('div');
-    buttonAddTodo.setAttribute('id', 'addTodo');
-    buttonAddTodo.addEventListener('click', () => {
-        modalAddTodo.showModal();
-    });
-    dashboard.appendChild(buttonAddTodo);
+    const getDashboard = () => dashboard;
+
+    function createButtonAddTodo() {
+        const buttonAddTodo = document.createElement('div');
+        buttonAddTodo.setAttribute('id', 'addTodo');
+        buttonAddTodo.addEventListener('click', () => {
+            modalAddTodo.showModal();
+        });
+        getDashboard().appendChild(buttonAddTodo);
+    }
+    createButtonAddTodo();
+
+    function resetDashboard() {
+        body.removeChild(dashboard);
+        dashboard = document.createElement('div');
+        dashboard.setAttribute('id', 'dashboard');
+        body.appendChild(dashboard);
+        createButtonAddTodo();
+    }
 
     buttonSubmitTodo.addEventListener('click', (button) => {
         button.preventDefault();
@@ -112,7 +143,7 @@ const dom = (function () {
         }
     });
 
-    return { body, sidebar, dashboard, modalAddProject, buttonAddProject};
+    return { body, sidebar, modalAddProject, buttonAddProject, resetDashboard, getDashboard };
 })();
 
-app.updateDisplay();
+app.initiateDisplay();
